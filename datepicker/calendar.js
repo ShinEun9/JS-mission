550b3f835c3e87e2c152fb7740d39c4a22db3be9
ShinEun9 <ses2201@naver.com>
2023-05-29 22:14:52 +0900

    0529 달력 주석 달기 및 에러 수정

--- datepicker/calendar.js
+++ datepicker/calendar.js
@@ -3,35 +3,35 @@ import monthNames from "./monthName.js";
 const days = document.querySelectorAll(".day");
 const calendar = document.querySelector(".calendar");
 const openBtn = document.querySelector(".calendar-open-btn");
+const backDrop = document.querySelector(".backdrop");
 const captionYear = document.querySelector(".year");
 const captionMonth = document.querySelector(".month");
 const timeEl = document.querySelector("time");
-const monthBtns = document.querySelectorAll('[class$="month"]');
 
-let calendarYear, calendarMonth, selectYear, selectMonth, selectDate;
-
+let calendarYear, calendarMonth;
+let selectYear, selectMonth, selectDate;
 const today = new Date();
 const todayYear = today.getFullYear(),
   todayMonth = today.getMonth() + 1,
   todayDate = today.getDate();
 
-function makingCalendar(newYear, newMonth) {
-  const time = new Date(newYear, newMonth - 1, 1); // 2023, 4, 1 -> 2023, 5, 1
+function makingCalendar(parameterYear, parmeterMonth) {
+  const time = new Date(parameterYear, parmeterMonth - 1, 1); // 2023, 5-1, 1 -> 2023,4,1 -> 2023년 5월 1일
   let year = time.getFullYear(),
-    month = time.getMonth(), // 4
+    month = time.getMonth() + 1,
     date = time.getDate(),
     day = time.getDay();
 
+  // * 달력 제목 수정
   captionYear.textContent = year;
   captionMonth.textContent = monthNames[month];
-  timeEl.dateTime = `${year}-${month + 1}`;
+  timeEl.dateTime = `${year}-${month}`;
 
-  const timeLength = new Date(year, month + 1, 0).getDate(); // 5, 0 -> 6월 0일 =  5월 30일
-  const lastMonthTimeLength = new Date(year, month, 0).getDate(); // 4 ,0 -> 5월 0일 = 4월 31일
+  // * 이번달이 며칠까지인지, 저번달이 며칠까지 인지 계산
+  const thisMonthTimeLength = new Date(year, month, 0).getDate(); // 5, 0 -> 6월 0일 = 5월 30일
+  const lastMonthTimeLength = new Date(year, month - 1, 0).getDate(); // 4 ,0 -> 5월 0일 = 4월 31일
 
-  let lastMonthDate = lastMonthTimeLength - day + 1;
-  let newMonthDate = 1;
-
+  // * 달력 그리기
   for (let i = 0; i < 42; i++) {
     days[i].classList.remove("prev-month-transparent");
     days[i].classList.remove("next-month-transparent");
@@ -40,34 +40,39 @@ function makingCalendar(newYear, newMonth) {
     days[i].classList.remove("today");
     days[i].classList.remove("not-hover");
 
+    // 이전 달 날짜 표시
     if (i < day) {
-      days[i].textContent = lastMonthDate++;
+      days[i].textContent = lastMonthTimeLength - day + 1 + i;
       days[i].classList.add("prev-month-transparent");
-    } else if (i < day + timeLength) {
+    }
+    // 해당 월 1일부터 말일까지 표시
+    else if (i < day + thisMonthTimeLength) {
       days[i].textContent = date++;
       if (i % 7 === 0) {
         days[i].classList.add("sunday");
       }
 
       // 선택한 날짜 표시
-      if (selectYear === year && selectMonth === month + 1) {
-        if (i === selectDate + (day - 1)) {
+      if (selectYear === year && selectMonth === month) {
+        if (i === day - 1 + selectDate) {
           days[i].classList.add("select-day");
         }
       }
 
       // 오늘 날짜 표시
-      if (todayYear === year && todayMonth === month + 1) {
-        if (i === todayDate + day - 1) {
+      if (todayYear === year && todayMonth === month) {
+        if (i === day - 1 + todayDate) {
           days[i].classList.add("today");
         }
       }
-    } else {
-      if (day + timeLength < 35 && i >= 35) {
+    }
+    // 다음달 날짜 표시
+    else {
+      if (day + thisMonthTimeLength < 35 && i >= 35) {
         days[i].innerHTML = "&nbsp;";
         days[i].classList.add("not-hover");
       } else {
-        days[i].textContent = newMonthDate++;
+        days[i].textContent = i + 1 - (day + thisMonthTimeLength);
         days[i].classList.add("next-month-transparent");
       }
     }
@@ -77,39 +82,48 @@ function makingCalendar(newYear, newMonth) {
 calendar.addEventListener("click", (event) => {
   openBtn.classList.remove("focus");
 
+  // * 날짜 버튼을 클릭했을 때
   if (event.target.classList.contains("day")) {
-    const captionMonthText = captionMonth.textContent;
-    const value = event.target.textContent;
+    const selectedDateValue = event.target.textContent;
 
     selectYear = parseInt(captionYear.textContent);
-    selectMonth = monthNames.findIndex((item) => item === captionMonthText) + 1;
-    selectDate = parseInt(value);
+    selectMonth = monthNames.findIndex(
+      (item) => item === captionMonth.textContent
+    );
+    selectDate = parseInt(selectedDateValue);
 
+    // 이전 달의 날짜 또는 다음 달의 날짜를 눌렀을 때 month를 조정해줘야한다.
     if (event.target.classList.contains("prev-month-transparent")) {
-      selectMonth = selectMonth - 1;
+      selectMonth -= 1;
+      if (selectMonth === 0) {
+        selectYear -= 1;
+        selectMonth = 12;
+      }
     } else if (event.target.classList.contains("next-month-transparent")) {
-      selectMonth = selectMonth + 1;
+      selectMonth += 1;
+      if (selectMonth === 13) {
+        selectYear += 1;
+        selectMonth = 1;
+      }
     }
 
     openBtn.value = openBtn.textContent = `${selectYear}-${
       selectMonth < 10 ? `0${selectMonth}` : selectMonth
     }-${selectDate < 10 ? `0${selectDate}` : selectDate}`;
 
-    console.log(openBtn.value);
-
     calendar.classList.remove("active");
+    console.log(openBtn.value);
   }
-});
 
-monthBtns.forEach((item) =>
-  item.addEventListener("click", (event) => {
-    if (event.currentTarget.classList.contains("prev-month")) {
+  // * 월 변경 버튼을 클릭했을 때
+  else if (event.target.closest("button")) {
+    if (event.target.closest("button").classList.contains("prev-month")) {
       makingCalendar(calendarYear, --calendarMonth);
     } else {
       makingCalendar(calendarYear, ++calendarMonth);
     }
-  })
-);
+  }
+});
 
 function calendarOpen() {
   openBtn.classList.add("focus");
@@ -122,13 +136,14 @@ function calendarOpen() {
   } else {
     calendarYear = selectYear;
     calendarMonth = selectMonth;
-    makingCalendar(selectYear, selectMonth, selectDate);
+    makingCalendar(calendarYear, calendarMonth);
   }
 }
 
-openBtn.addEventListener("click", calendarOpen);
-
-document.querySelector(".backdrop").addEventListener("click", () => {
+function calendarClose() {
   openBtn.classList.remove("focus");
   calendar.classList.remove("active");
-});
+}
+
+openBtn.addEventListener("click", calendarOpen);
+backDrop.addEventListener("click", calendarClose);
--- datepicker/monthName.js
+++ datepicker/monthName.js
@@ -1,4 +1,5 @@
 const monthNames = [
+  "",
   "January",
   "February",
   "March",
